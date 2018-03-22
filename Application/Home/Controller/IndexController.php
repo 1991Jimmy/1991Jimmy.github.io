42c6955862dfb4ea0396762ba09e972457673de0
Jimmy <1272425732@qq.com>
2018-03-22 14:07:55 +0800

    添加了一个控制器

---
+++ Application/Home/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController {
+	public function index(){
+		$this->display();
+	}
+	public function artists(){
+		$this->display();
+	}
+	public function blog(){
+		$this->display();
+	}
+	public function contact(){
+		$this->display();
+	}
+}
