1e020a9a220e4fbbaf69a42d9549c4970ead6097
Jimmy <1272425732@qq.com>
2018-03-22 18:17:55 +0800

    修改样式文件

--- Application/Public/Static/admin/js/realm.js
+++ Application/Public/Static/admin/js/realm.js
@@ -28,7 +28,7 @@ $(function() {
       html:true,
       placement: 'bottom',
       title: 'Messages',
-      content:'<ul><li><a href="#"><img src="assets/img/avatars/04.jpg"><span>Today</span><h4>Bill Fernando</h4>Hey John, have you finished the report yet?</a></li><li><a href="#"><img src="assets/img/avatars/05.jpg"><span>Yesterday</span><h4>Roger White</h4>Did you talk to Andrea? She was looking for you</a></li></ul><div class="popover_footer"><a href="">View all messages</a></div>'
+      content:'<ul><li><a href="#"><img src="/img/avatars/04.jpg"><span>Today</span><h4>Bill Fernando</h4>Hey John, have you finished the report yet?</a></li><li><a href="#"><img src="/img/avatars/05.jpg"><span>Yesterday</span><h4>Roger White</h4>Did you talk to Andrea? She was looking for you</a></li></ul><div class="popover_footer"><a href="">View all messages</a></div>'
     });
 
     // header menu notifications 
@@ -36,7 +36,7 @@ $(function() {
       html:true,
       placement: 'bottom',
       title: 'Notifications',
-      content:'<ul><li><a href="#"><img src="assets/img/avatars/07.jpg"><span>Today</span><h4>Cody Negley</h4>Requested a vacation for 3 days.</a></li><li><a href="#"><img src="assets/img/avatars/09.jpg"><span>Yesterday</span><h4>Customer Support</h4>You have 3 new tickets assigned to you.</a></li></ul><div class="popover_footer"><a href="">View all notifications</a></div>'
+      content:'<ul><li><a href="#"><img src="/img/avatars/07.jpg"><span>Today</span><h4>Cody Negley</h4>Requested a vacation for 3 days.</a></li><li><a href="#"><img src="/img/avatars/09.jpg"><span>Yesterday</span><h4>Customer Support</h4>You have 3 new tickets assigned to you.</a></li></ul><div class="popover_footer"><a href="">View all notifications</a></div>'
     });
 
 
